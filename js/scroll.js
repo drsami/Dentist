2c248309e73def0ee995f4c66f7062c9638f60b7
Nahian <nahian007@gmail.com>
2013-07-06 12:02:09 +0600

    This this ADO version 1.1

---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+(function($){
+			$(window).load(function(){
+				$("#content_1").mCustomScrollbar({
+					scrollButtons:{
+						enable:true
+					}
+				});
+				//ajax demo fn
+				$("a[rel='load-content']").click(function(e){
+					e.preventDefault();
+					var $this=$(this),
+						url=$this.attr("href");
+					$this.addClass("loading");
+					$.get(url,function(data){
+						$this.removeClass("loading");
+						$("#content_1 .mCSB_container").html(data); //load new content inside .mCSB_container
+						$("#content_1").mCustomScrollbar("update"); //update scrollbar according to newly loaded content
+						$("#content_1").mCustomScrollbar("scrollTo","top",{scrollInertia:200}); //scroll to top
+					});
+				});
+				$("a[rel='append-content']").click(function(e){
+					e.preventDefault();
+					var $this=$(this),
+						url=$this.attr("href");
+					$this.addClass("loading");
+					$.get(url,function(data){
+						$this.removeClass("loading");
+						$("#content_1 .mCSB_container").append(data); //append new content inside .mCSB_container
+						$("#content_1").mCustomScrollbar("update"); //update scrollbar according to newly appended content
+						$("#content_1").mCustomScrollbar("scrollTo","h2:last",{scrollInertia:2500,scrollEasing:"easeInOutQuad"}); //scroll to appended content
+					});
+				});
+			});
+		})(jQuery);
